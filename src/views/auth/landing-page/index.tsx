4ac47d16a70445e49c1adc59bda5aaf2424aa7ff
bob <sardorbekerkinov03@gmail.com>
2022-06-14 01:05:48 +0500

    initializing landing page

---
+++ src/views/auth/landing-page/index.tsx
@@ -0,0 +1,12 @@
+import CreateLoad from './create-load';
+import { LandingPageWrapper } from './landing-page.styles';
+
+const LandingPage = () => {
+  return (
+    <LandingPageWrapper>
+      <CreateLoad />
+    </LandingPageWrapper>
+  );
+};
+
+export default LandingPage;
