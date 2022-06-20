8fa3c0e893198b26e55058103544904de3ab712f
Bekhzod <bekhzodaliakbarov@gmail.com>
2022-06-20 22:41:56 +0500

    Route security implemented

--- src/views/app/app-views.tsx
+++ src/views/app/app-views.tsx
@@ -1,21 +1,36 @@
 import ProfileLayout from 'layouts/profile-layout/profile-layout';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Feed from './feed/feed';
 import ActionLoadLayout from 'layouts/load-action-layout';
 import SearchDriverLayout from 'layouts/search-driver-layout';
 import DriverInfoLayout from 'layouts/driver-info-layout/driver-info-layout';
 import SearchLoadLayout from 'layouts/search-load-layout';
+import { useDriver } from 'hooks/use-driver';
 
-const AppViews = () => (
+const CustomerView = () => (
   <Routes>
     <Route path="/" element={<Feed />} />
+    <Route path="/profile/*" element={<ProfileLayout />} />
     <Route path="/create-load" element={<ActionLoadLayout />} />
     <Route path="/edit-load" element={<ActionLoadLayout />} />
-    <Route path="/profile/*" element={<ProfileLayout />} />
     <Route path="/search-driver/*" element={<SearchDriverLayout />} />
-    <Route path="/search-load/*" element={<SearchLoadLayout />} />
     <Route path="/drivers/:id" element={<DriverInfoLayout />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
   </Routes>
 );
+
+const DriverView = () => (
+  <Routes>
+    <Route path="/search-load/*" element={<SearchLoadLayout />} />
+    <Route path="/" element={<Feed />} />
+    <Route path="/profile/*" element={<ProfileLayout />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes>
+);
+
+const AppViews = () => {
+  const { isDriver } = useDriver();
+  return <>{isDriver ? <DriverView /> : <CustomerView />}</>;
+};
 
 export default AppViews;
