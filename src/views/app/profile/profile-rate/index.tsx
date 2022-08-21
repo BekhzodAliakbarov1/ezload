4c9f9e321c8b07fede3362c46bbf8d2fef5bbe8b
Bekhzod <bekhzodaliakbarov@gmail.com>
2022-08-21 08:19:56 +0500

    rates skeloton implemented

--- src/views/app/profile/profile-rate/index.tsx
+++ src/views/app/profile/profile-rate/index.tsx
@@ -1,6 +1,7 @@
 import ReviewCard from 'components/cards/review-card';
 import FileIcon from 'components/icons/file.icon';
 import RatingComponent from 'components/rating/rating';
+import RateContainerSkeloton from 'components/skelotons/rate-container';
 import Text from 'components/typography/text';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -16,7 +17,7 @@ import {
 } from './profile-rate.styles';
 
 const ProfileRate = () => {
-  const { data } = useRates();
+  const { data, isLoading } = useRates();
   const { t } = useTranslation();
 
   return (
@@ -24,32 +25,41 @@ const ProfileRate = () => {
       <Text>{t('Rates & Testimonials')}</Text>
       <ProfileRatesStarsWrapper>
         <Text weight="700">{t('Overall rate')}</Text>
-        <OveralRatingDataWrapper>
-          <RatingComponent value={data?.average_rate ?? 0} iconSize="35px" />
-          <Text weight="700">{data?.average_rate}</Text>
-        </OveralRatingDataWrapper>
-        <DifferentRatingsBox>
-          <RatingsRaw>
-            <RatingComponent value={5} iconSize="24px" />
-            <Text weight="500">{data?.rate_five}</Text>
-          </RatingsRaw>
-          <RatingsRaw>
-            <RatingComponent value={4} iconSize="24px" />
-            <Text weight="500">{data?.rate_four}</Text>
-          </RatingsRaw>
-          <RatingsRaw>
-            <RatingComponent value={3} iconSize="24px" />
-            <Text weight="500">{data?.rate_three}</Text>
-          </RatingsRaw>
-          <RatingsRaw>
-            <RatingComponent value={2} iconSize="24px" />
-            <Text weight="500">{data?.rate_two}</Text>
-          </RatingsRaw>
-          <RatingsRaw>
-            <RatingComponent value={1} iconSize="24px" />
-            <Text weight="500">{data?.rate_one}</Text>
-          </RatingsRaw>
-        </DifferentRatingsBox>
+        {isLoading ? (
+          <RateContainerSkeloton />
+        ) : (
+          <>
+            <OveralRatingDataWrapper>
+              <RatingComponent
+                value={data?.average_rate ?? 0}
+                iconSize="35px"
+              />
+              <Text weight="700">{data?.average_rate}</Text>
+            </OveralRatingDataWrapper>
+            <DifferentRatingsBox>
+              <RatingsRaw>
+                <RatingComponent value={5} iconSize="24px" />
+                <Text weight="500">{data?.rate_five}</Text>
+              </RatingsRaw>
+              <RatingsRaw>
+                <RatingComponent value={4} iconSize="24px" />
+                <Text weight="500">{data?.rate_four}</Text>
+              </RatingsRaw>
+              <RatingsRaw>
+                <RatingComponent value={3} iconSize="24px" />
+                <Text weight="500">{data?.rate_three}</Text>
+              </RatingsRaw>
+              <RatingsRaw>
+                <RatingComponent value={2} iconSize="24px" />
+                <Text weight="500">{data?.rate_two}</Text>
+              </RatingsRaw>
+              <RatingsRaw>
+                <RatingComponent value={1} iconSize="24px" />
+                <Text weight="500">{data?.rate_one}</Text>
+              </RatingsRaw>
+            </DifferentRatingsBox>
+          </>
+        )}
       </ProfileRatesStarsWrapper>
       <Text className="reviews" weight="700">
         {t('All user reviews')}
